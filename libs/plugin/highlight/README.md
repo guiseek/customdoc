f24c95f2770c2392c70cc74da8ffe29ed162f330
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-08-18 20:40:54 -0300

    chore(compodoc): removido plugin sem uso
    adicionaremos depois plugins para scully

--- libs/plugin/highlight/README.md
+++
@@ -1,7 +0,0 @@
-# plugin-highlight
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `ng test plugin-highlight` to execute the unit tests via [Jest](https://jestjs.io).
--- libs/plugin/highlight/src/lib/plugin-highlight.spec.ts
+++
@@ -1,7 +0,0 @@
-import { pluginHighlight } from './plugin-highlight';
-
-describe('pluginHighlight', () => {
-  it('should work', () => {
-    expect(pluginHighlight()).toEqual('plugin-highlight');
-  });
-});
