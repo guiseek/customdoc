025c1e39e412e8db64aff84106af93887e47eb50
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-08-21 20:31:46 -0300

    fix(repo): fix release

--- libs/common/compodoc-viewer/.releaserc.js
+++ libs/common/compodoc-viewer/.releaserc.js
@@ -5,10 +5,3 @@ module.exports = createReleaseConfigWithScopeFilter({
   projectRoot: 'libs/common/compodoc-viewer',
   buildOutput: 'dist/libs/common/compodoc-viewer',
 });
-
-// const config = createReleaseConfigWithScopeFilter({
-//   projectScope: 'common-compodoc-viewer',
-//   projectRoot: 'libs/common/compodoc-viewer',
-//   buildOutput: 'dist/libs/common/compodoc-viewer',
-// });
-// console.log(JSON.stringify(config));
---
+++ libs/common/paginator-store/src/utils/versions.ts
@@ -0,0 +1,3 @@
+export const versions = {
+  '@customdoc/paginator-store': '*'
+};
